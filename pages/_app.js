af220f87c5ed605df57fb3bc5091d937728f2a57
NgYouJuin <ngyoujuin@yahoo.com>
2021-04-18 17:18:39 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-toastify/dist/ReactToastify.css';
+import 'slate-simple-editor/dist/index.css'
+import '@/styles/main.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
